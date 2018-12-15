c2d21b70d13f80f0e54125dfd1d91d1dd5b68105
Kapil Neurgaonkar (PES) <kapiln2@kpit.com>
2018-12-15 20:30:31 +0530

    Handled auth redirect

--- app/index.js
+++ app/index.js
@@ -105,7 +105,24 @@ app.post('/moz/url', function (req, res) {
 });
 
 app.post('/auth/redirect', function (req, res) {
-    
+    var options = {
+        uri: 'https://slack.com/api/oauth.access?code='
+            +req.query.code+
+            '&client_id='+process.env.CLIENT_ID+
+            '&client_secret='+process.env.CLIENT_SECRET+
+            '&redirect_uri='+process.env.REDIRECT_URI,
+        method: 'GET'
+    }
+    request(options, (error, response, body) => {
+        var JSONresponse = JSON.parse(body)
+        if (!JSONresponse.ok){
+            console.log(JSONresponse)
+            res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+        }else{
+            console.log(JSONresponse)
+            res.send("Success!")
+        }
+    })
 });
 
 app.use('/', express.static(path.join(__dirname, 'public')));
